1518ced919bf8fd35b79aa2d40221f606223aa13
David Cady Jr <davidcadyjr@mac.com>
2017-07-19 16:43:34 -0500

    created html and compnent.ts

--- GOT/src/app/app.component.ts
+++ GOT/src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+class Character{
+	id: number;
+	first_name: string;
+	last_name: string;
+	house: string;
+	dead: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,10 +16,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  house = {};
+  newCharacter: Character = new Character();
+  currentCharacter: Character = new Character();
+
+  characters: Character[] = [];
+  baseApiUrl: string = 'http://localhost:9393/characters/1';
+
+  constructor(private http: Http){
+  	this.getCharacters();
+  }
+
+  getCharacters(){
+  	this.http.get(this.baseApiUrl).subscribe(response =>
+  		this.characters = response.json()
+  		)
+  }
+
 
   onSubmit(){
-  	console.log(this.house);
+  	console.log();
   }
 
 }
