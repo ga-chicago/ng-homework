7b0a9cef6e4d3de0c3c3043982c650bccc0223e7
Jackson Tenclay <jptenclay@gmail.com>
2017-07-19 16:07:15 -0500

    complete

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 class Person {
 	id: number;
-	name: string;
+	first_name: string;
+  last_name: string;
 	room_number: number;
-	floor_id: number;
+	favorite_food: string;
 }
 
 @Component({
@@ -12,11 +14,43 @@ class Person {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  person: Person = new Person();
-  books: Book[] = []
 
-  submitBook() {
-  	this.books.push(this.book);
+export class AppComponent {
+  newPerson: Person = new Person();
+  editPerson: Person = new Person();
+  people: Person[] = [];
+  baseApiUrl: string = 'http://localhost:9393/people/';
+
+  constructor(private http: Http) {
+    this.getPeople();
   }
+
+  getPeople() {
+    this.http.get(this.baseApiUrl).subscribe(response =>
+      this.people = response.json()
+    )
+  }
+
+  postPerson() {
+    this.http.post(this.baseApiUrl, this.newPerson).subscribe(response =>
+      this.people.push(response.json())
+    )
+  }
+
+  selectPerson(person) {
+    this.editPerson = Object.assign({}, person);
+  }
+
+  patchPerson() {
+    this.http.patch(this.baseApiUrl + this.editPerson.id, this.editPerson).subscribe(response =>
+      this.people = response.json()
+    )
+  }
+
+  deletePerson(person) {
+    this.http.delete(this.baseApiUrl + person.id).subscribe(response =>
+      this.people = response.json()
+    )
+  }
+  
 }
