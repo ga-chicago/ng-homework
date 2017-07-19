4782ce3e353390e204e44a1409368c17823d242e
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-19 16:33:07 -0500

    compiling correctly

--- app/client/models/MenuModel.rb
+++
@@ -1,5 +0,0 @@
-class Menu < ActiveRecord::Base
-	self.table_name = 'menu'
-	has_many :reservations, class_name: 'Reservation'
-	has_many :menus, through: :reservations
-end
--- app/client/models/RestaurantModel.rb
+++
@@ -1,5 +0,0 @@
-class Restaurant < ActiveRecord::Base
-	self.table_name = 'restaurants'
-	has_many :reservations, class_name: 'Reservation'
-	has_many :menus, through: :reservations
-end
