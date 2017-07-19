82ca87078cecfa92b586bd88aa22f6a24b55a4cc
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-19 17:26:25 -0500

    backend uploaded

--- app/client/src/app/app.component.ts
+++ app/client/src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
 	newRestaurant: Restaurant = new Restaurant ();
 	currentRestaurant: Restaurant = new Restaurant();
 	restaurants: Restaurant [] = [];
-	baseApiUrl: string = 'http://localhost:4200/';
+	baseApiUrl: string = 'http://localhost:9393/restaurants';
 
 	constructor(private http: Http){
 		this.getRestaurants();
@@ -35,25 +35,29 @@ export class AppComponent {
 
 
 	postRestaurant(){
-		this.http.get(this.baseApiUrl).subscribe(response =>
+		this.http.post(this.baseApiUrl, this.newRestaurant).subscribe(response =>
 			this.restaurants.push(response.json())
 			)
 	}
+
+
+	patchRestaurant(){
+		this.http.patch(this.baseApiUrl + this.currentRestaurant.id, this.currentRestaurant).subscribe(response =>
+      		this.restaurants = response.json()
+			)
+	}
+
+	destroyRestaurant(restaurant){
+		this.http.delete(this.baseApiUrl + restaurant.id).subscribe(response =>
+      		this.restaurants = response.json()
+			)
+	}
+
+	editRestaurant(restaurant){
+		this.currentRestaurant = Object.assign({}, restaurant);
+	}
+
+	onSubmit(){
+		console.log(this.newRestaurant);
+	}
 }
-
-// 	patchRestaurant(){
-
-// 	}
-
-// 	destroyRestaurant(restaurant){
-
-// 	}
-
-// 	editRestaurant(restaurant){
-
-// 	}
-
-// 	onSubmit(){
-// 		console.log(this.newRestaurant);
-// 	}
-// }
