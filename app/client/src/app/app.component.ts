3a45599656c71ccc90ad01ae8c0d2c2cee568d18
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-19 17:12:32 -0500

    html and ts rendering

--- app/client/src/app/app.component.ts
+++ app/client/src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+
+class Restaurant {
+	id: number;
+	name: string;
+	address: string;
+	cuisine_type: string;
+	company: string;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+	newRestaurant: Restaurant = new Restaurant ();
+	currentRestaurant: Restaurant = new Restaurant();
+	restaurants: Restaurant [] = [];
+	baseApiUrl: string = 'http://localhost:4200/';
+
+	constructor(private http: Http){
+		this.getRestaurants();
+	}
+
+	getRestaurants(){
+		this.http.get(this.baseApiUrl).subscribe(response =>
+			this.restaurants = response.json()
+			)
+	}
+
+
+	postRestaurant(){
+		this.http.get(this.baseApiUrl).subscribe(response =>
+			this.restaurants.push(response.json())
+			)
+	}
 }
+
+// 	patchRestaurant(){
+
+// 	}
+
+// 	destroyRestaurant(restaurant){
+
+// 	}
+
+// 	editRestaurant(restaurant){
+
+// 	}
+
+// 	onSubmit(){
+// 		console.log(this.newRestaurant);
+// 	}
+// }
